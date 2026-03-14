421763a2dd4234a56042cc163924acc21954de1f
DevOps Engineer <123374733+AniketFirke@users.noreply.github.com>
2026-03-14 12:09:38 +0530

    Delete DevOps/terraform-aws-ec2 directory

--- DevOps/terraform-aws-ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "EC2 Instance ID"
-  value       = aws_instance.web.id
-}
-
-output "public_ip" {
-  description = "Public IP Address"
-  value       = aws_instance.web.public_ip
-}
